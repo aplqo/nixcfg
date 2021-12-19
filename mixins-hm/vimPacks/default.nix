28561e49fe7f3aecc1780b9ee9a06d8d5f45ac71
aplqo <aplqo@outlook.com>
2021-12-19 02:46:03 +0000

    [shell, modules-hm] rust: Add clippy and format

--- mixins-hm/vimPacks/default.nix
+++ mixins-hm/vimPacks/default.nix
@@ -70,7 +70,10 @@ in rec {
       base.extraPackages = [ pkgs.rust-analyzer ];
       coc = {
         extensions = [ pkgs.vimPlugins.coc-rust-analyzer ];
-        config."rust-analyzer.server.path" = "${pkgs.rust-analyzer.outPath}/bin/rust-analyzer";
+        config = {
+          "rust-analyzer.server.path" = "${pkgs.rust-analyzer.outPath}/bin/rust-analyzer";
+          "coc.preferences.formatOnSaveFiletypes" = [ "rust" ];
+        };
       };
     });
   };
--- shells/default.nix
+++ shells/default.nix
@@ -4,6 +4,8 @@
     nativeBuildInputs = [
       pkgs.cargo
       pkgs.rustc
+      pkgs.clippy
+      pkgs.rustfmt
       pkgs.gcc
     ];
     RUST_SRC_PATH = pkgs.rust.packages.stable.rustPlatform.rustLibSrc;
