ac3256aa92a70e46a2031d6dc0cd9d11c9ecfdf6
aplqo <aplqo@outlook.com>
2021-12-19 07:10:23 +0000

    [mmodules-hm] vimPacks/languages: add dhall

--- mixins-hm/vimPacks/languages/default.nix
+++ mixins-hm/vimPacks/languages/default.nix
@@ -13,6 +13,19 @@
     };
   });
 
+  dhall = lib.mkPack ({pkgs, ...}: {
+    base = {
+      plugins = [ pkgs.vimPlugins.dhall-vim ];
+      extraPackages = [ pkgs.dhall-lsp-server ];
+    };
+    coc.config = {
+      languageserver.dhall = {
+        command = "dhall-lsp-server";
+        filetypes = [ "dhall" ];
+      };
+    };
+  });
+
   haskell = lib.mkPack ({pkgs, ...}: {
     base = {
       plugins = [ pkgs.vimPlugins.haskell-vim ];
