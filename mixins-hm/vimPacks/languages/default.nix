629e93b2c74ed81461503dc6950ee68f540418ca
aplqo <aplqo@outlook.com>
2021-12-19 06:56:15 +0000

    [modules-hm] vimPacks/language: add scala

--- mixins-hm/vimPacks/languages/default.nix
+++ mixins-hm/vimPacks/languages/default.nix
@@ -62,6 +62,17 @@
     };
   });
 
+  scala = lib.mkPack ({pkgs, ...}: {
+    coc = {
+      extensions = [ pkgs.vimPlugins.coc-metals ];
+      config = {
+        "metals.javaHome" = "${pkgs.openjdk_headless.outPath}/lib/openjdk";
+        "metals.statusBarEnabled" = true;
+        "coc.preferences.formatOnSaveFiletypes" = [ "scala" ];
+      };
+    };
+  });
+
   yaml = lib.mkPack ({pkgs, ...}: {
     coc = {
       extensions = [ pkgs.vimPlugins.coc-yaml ];
