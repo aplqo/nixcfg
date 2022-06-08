0eb74f0b75d4f49f5cb2110355da367923cf9a79
aplqo <aplqo@outlook.com>
2022-06-08 10:10:12 +0800

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -1,21 +1,26 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     home-manager.url = "github:nix-community/home-manager";
   };
 
   outputs = {nixpkgs, home-manager, ...} @inputs: rec {
-    nixosConfigurations = {
-      nixos-dev1 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ 
-          ./hosts/nixos-dev1/configuration.nix 
+    nixosConfigurations = let
+      mkSystem = name: system: nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hosts/${name}/configuration.nix
           home-manager.nixosModules.home-manager
           ({pkgs, ...}: {
-            nixpkgs.overlays = [ overlays.vimPlugins ]; 
+            nixpkgs.overlays = [ overlays.vimPlugins ];
           })
         ];
         specialArgs = { inherit inputs; };
       };
+    in {
+      nixos-dev1 = mkSystem "nixos-dev1" "x86_64-linux";
+
+      nixos-laptop1 = mkSystem "nixos-laptop1" "x86_64-linux";
     };
 
     overlays = import ./overlays;
