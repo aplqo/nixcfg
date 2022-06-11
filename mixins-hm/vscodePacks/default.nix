2489e46de7826295d783dfc67297ef104e703d60
aplqo <aplqo@outlook.com>
2022-06-11 18:02:18 +0800

    [mixins-hm] vscodePacks: add languages.antlr4

--- mixins-hm/vscodePacks/default.nix
+++ mixins-hm/vscodePacks/default.nix
@@ -21,6 +21,15 @@
   };
 
   language = {
+    antlr4 = {pkgs, ...}: {
+      programs.vscode = {
+        extensions = [ pkgs.vscode-extensions.mike-lischke.vscode-antlr4 ];
+        userSettings = {
+          "antlr4.referencesCodeLens.enabled" = true;
+        };
+      };
+    };
+
     scala = {pkgs, ...}: {
       programs.vscode = {
         extensions = with pkgs.vscode-extensions; [
