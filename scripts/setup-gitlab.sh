8f67c2cb72dfa561f2052b23916bc63ac307ddb9
aplqo <aplqo@outlook.com>
2022-06-16 19:30:59 +0800

    [scripts] setup-gitlab: fix generate

--- scripts/setup-gitlab.sh
+++ scripts/setup-gitlab.sh
@@ -2,19 +2,19 @@
 
 set -o errexit
 
-readonly bits=32
+readonly bits=128
 readonly max_length=200
 
 mkdir -p /var/keys/gitlab
 cd /var/keys/gitlab
 
 generate_password() {
-  head -c $bits | base64 -w $max_length > $1
+  head -c $bits /dev/random | base64 -w $max_length > $1
 }
 
 generate_password root_password
 generate_password db
-generate_password db_passowrd
+generate_password db_password
 generate_password otp
 generate_password secret
 
