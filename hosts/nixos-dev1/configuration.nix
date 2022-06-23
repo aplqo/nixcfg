a44dbd73fb66dd4db445e7706ea20f0b0c40cbcb
aplqo <aplqo@outlook.com>
2022-06-23 07:57:41 +0000

    [host] nixos-dev1: move common hm user into common

--- hosts/nixos-dev1/configuration.nix
+++ hosts/nixos-dev1/configuration.nix
@@ -6,33 +6,42 @@
     ../../mixins/server-user.nix
   ];
 
-  home-manager.users.aplqo = {pkgs, ...}: {
-    imports = [
-      ../../mixins-hm/tmux.nix
-      (import ../../mixins-hm/htop.nix { profile = "server"; })
+  home-manager.users = let
+    common = name: email: {pkgs, ...}: {
+      imports = [
+        (import ../../mixins-hm/git.nix name email)
+        ../../mixins-hm/tmux.nix
+        (import ../../mixins-hm/htop.nix { profile = "server"; })
 
       # vim packs
-      ../../modules-hm/xvim
-      (let
-        packs = import ../../mixins-hm/vimPacks;
-      in {pkgs, ...}: {
-        imports = [ 
-          ((import ../../modules-hm/xvim/lib.nix).evalPacks {
-            all = with packs; [ 
-              profiles.basic
-              languages.nix 
-              misc.lightline
-            ];
-          }) 
-        ];
+        ../../modules-hm/xvim
+        (let
+          packs = import ../../mixins-hm/vimPacks;
+        in {pkgs, ...}: {
+          imports = [ 
+            ((import ../../modules-hm/xvim/lib.nix).evalPacks {
+              all = with packs; [ 
+                profiles.basic
+                languages.nix 
+                misc.lightline
+              ];
+            }) 
+          ];
 
-        config = {
-          modules.xvim = {
-            neovim.base.enable = true;
+          config = {
+            modules.xvim = {
+              neovim.base.enable = true;
+            };
           };
-        };
-      })
-    ];
-
+        })
+      ];
+    };
+  in {
+    aplqo = {
+      imports = [
+        (common "aplqo" "aplqo@outlook.com")
+      ];
+    };
   };
+
 }
